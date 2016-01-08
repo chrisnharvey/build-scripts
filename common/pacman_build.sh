3aa40c410a4c552c768a3a3e97576e6759b2fa03
Chris Harvey <chris@chrisnharvey.com>
2016-01-08 22:36:03 +0000

    Replace hardcoded app name (audacity) with $APPNAME variable

--- common/pacman_build.sh
+++ common/pacman_build.sh
@@ -2,7 +2,7 @@
 
 rm -r /tmp/appbuild/$APPNAME
 mkdir -p /tmp/appbuild/$APPNAME/pkg /tmp/appbuild/$APPNAME/files
-pacman -Sy audacity --dbpath /var/lib/pacman --cachedir /tmp/appbuild/$APPNAME/pkg --config $APPDIR/../common/pacman.conf --noconfirm --downloadonly
+pacman -Sy $APPNAME --dbpath /var/lib/pacman --cachedir /tmp/appbuild/$APPNAME/pkg --config $APPDIR/../common/pacman.conf --noconfirm --downloadonly
 ls /tmp/appbuild/$APPNAME/pkg/*.tar* | xargs -i tar xf {} -C /tmp/appbuild/$APPNAME/files
 cp -R $APPDIR/AppDir /tmp/appbuild/$APPNAME
 mv -v /tmp/appbuild/$APPNAME/files/* /tmp/appbuild/$APPNAME/AppDir
